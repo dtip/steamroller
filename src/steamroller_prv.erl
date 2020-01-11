6e40cf5e6a434b25be2366163affd14ce01a764c
Daniel Tipping <daniel@oldreliable.tech>
2020-01-11 15:28:09 +0000

    Remove check that steamroller_worker_sup started correctly
    When we format multiple apps we try to start the worker sup multiple
    times. This change prevents a crash.
    
    Thanks to @tommysanterre for reporting this bug.

--- src/steamroller_prv.erl
+++ src/steamroller_prv.erl
@@ -118,7 +118,7 @@ find_dir_files(Dir) ->
 
 format_files(Files, Opts) ->
     {Headers, OtherFiles} = steamroller_utils:split_header_files(Files),
-    {ok, _} = steamroller_worker_sup:start_link(Opts),
+    steamroller_worker_sup:start_link(Opts),
     J = proplists:get_value(j, Opts, ?DEFAULT_J_FACTOR),
     case J of
         J when J > 1 ->
